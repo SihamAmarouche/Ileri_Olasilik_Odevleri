06a43c37cb8c11b12bb89249bbc3ad79a0987b0b
Siham Amaouche <10710980+SihamAmarouche@users.noreply.github.com>
2019-03-07 15:28:31 +0300

    Update Guvercin_Yuvasi_Odev1.py

--- Odev 1/Guvercin_Yuvasi_Odev1.py
+++ Odev 1/Guvercin_Yuvasi_Odev1.py
@@ -3,24 +3,23 @@ import math;
 
 #Fonksiyonlar
 
-def InputKontrolu(GirilenKisiSayisi): # Bir harf girildiğinde veya bir negatif sayı girildiğinde matematik işlemleri yapılmaz hata verir
-    deger = str(GirilenKisiSayisi)
-    if not deger.isdigit(): # Girilen değer bir sayı olup olmadığını kontrol ediliyor
-        return False
-    elif (int(deger)<1 ) or (int(deger)>96): # Verilen aralık :1 ile 96 kişi arasında olması kontrolu
-        return False
-    else:
-        return True
-
 def Result(Kisi_Sayisi): #Güvercin Yuvası Prensibi Uygulanıyor ve odadaki toplanan kişi sayısı haftanın 7 gününe bölünür
 
     result=(math.ceil(int(Kisi_Sayisi)/7.0)) # 7.0 değer haftanın günlerine ait
 
     return result
 
+def InputKontrolu(GirilenKisiSayisi): # Bir harf girildiğinde veya bir negatif sayı girildiğinde matematik işlemleri yapılmaz hata verir
+    value = str(GirilenKisiSayisi)
+    if not value.isdigit(): # Girilen değer bir sayı olup olmadığını kontrol ediliyor
+        return False
+    elif (int(value)<1 ) or (int(value)>96): # Verilen aralık :1 ile 96 kişi arasında olması kontrolu
+        return False
+    else:
+        return True
 
 while True:
-    print("Lütfen 1 ile 76 aralığında bir sayı giriniz !!")
+    print("Lütfen 1 ile 96 aralığında bir sayı giriniz !!")
     Kisi_Sayisi=input("Odada toplamda kaç kişi bulunmakta ? ")
 
     if InputKontrolu(Kisi_Sayisi) == True:
@@ -28,4 +27,3 @@ while True:
     else:
         print("İstenilen aralıkta bir sayı giriniz lütfen !!")
     print("\n")
-
